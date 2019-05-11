d2856e6e1f672a0a301c616ddcdaf88fa0cc13f6
dingyao <1915552319@qq.com>
2019-05-11 11:33:28 +0800

    tired

---
+++ OS/ring.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+typedef struct {
+    int value;
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+} sema_t;
+
+void sema_init(sema_t *sema, int value)
+{
+    sema->value = value;
+    pthread_mutex_init(&sema->mutex, NULL);
+    pthread_cond_init(&sema->cond, NULL);
+}
+
+void sema_wait(sema_t *sema)
+{
+    pthread_mutex_lock(&sema->mutex);
+    sema->value--;
+    while (sema->value < 0)
+        pthread_cond_wait(&sema->cond, &sema->mutex);
+    pthread_mutex_unlock(&sema->mutex);
+}
+
+void sema_signal(sema_t *sema)
+{
+    pthread_mutex_lock(&sema->mutex);
+    ++sema->value;
+    pthread_cond_signal(&sema->cond);
+    pthread_mutex_unlock(&sema->mutex);
+}
